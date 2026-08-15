d2f761a52517d2b0a5516e21f4291fb7ecae1c40
nghia <nghiatrieukhuc@gmail.com>
2026-08-15 14:28:47 -0700

    baitap2

---
+++ bai 2 c++.cpp
@@ -0,0 +1,19 @@
+// bai 2 c++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	printf("nhap a: ");
+	scanf_s("%d", &a);
+	printf("nhap b: ");
+	scanf_s("%d", &b);
+	printf("Tong= %d", a + b);
+	printf("Hieu=%d", a - b);
+	printf("Tich=%d", a * b);
+	printf("Thuong=%d", a / b);
+
+
+}
