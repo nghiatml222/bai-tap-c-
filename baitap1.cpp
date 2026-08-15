9752402d3645fb45cbd975fc33b6dcc795feed2a
nghia <nghiatrieukhuc@gmail.com>
2026-08-15 14:07:06 -0700

    bai tap c++

---
+++ baitap1.cpp
@@ -0,0 +1,11 @@
+// baitap1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+#define TEN "Nguyen_Dai_Nghia"
+#define NAM_SINH 2006
+int main()
+{
+    int TUOI = 2026 - NAM_SINH;
+    printf("Hello!My name is %s. I am %d years old.", TEN, TUOI);
+}
